107ac4accc2f73c778bb90169f5aa8d1f79b7450
oitowl7 <shear_j@hotmail.com>
2018-04-01 13:29:04 -0400

    mostly empty project ready to be worked on. also we will now see if react is working on heroku

---
+++ client/src/components/WeHaveAnEmptyProject/WeHaveAnEmptyProject.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container, Header, Icon } from 'semantic-ui-react'
+import "./WeHaveAnEmptyProject.css";
+
+
+const WeHaveAnEmptyProject = () => (
+    <div>
+        <Header textAlign="center">
+            This project is currently empty. But this component is loading which means that react is working. Also, if it centers, it means that semantic is working too.
+        </ Header>
+    </div>
+  )
+
+export default WeHaveAnEmptyProject
--- client/src/keys.js
+++
@@ -1,3 +0,0 @@
-exports.keys = {
-	nyt: "46dbd7b690f34bad8709b270648f8dea"
-};
