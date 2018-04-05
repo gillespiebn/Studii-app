1ee24ea94e8f6dbd41011b540062227196a10fe2
Mary Lorraine Hall <mh6uj@virginia.edu>
2018-04-05 19:17:41 -0400

    class and professor in questionnaire

--- client/src/components/Questionnaire/Questionnaire.js
+++ client/src/components/Questionnaire/Questionnaire.js
@@ -28,6 +28,14 @@ class Questionnaire extends Component {
         console.log(this.state.classStanding);
        };
 
+       updateClassID = event => {
+        event.preventDefault();
+            var classIDs=this.state.classID
+            classIDs.push(event.target)
+            this.setState({ classID: classIDs})
+                console.log("State " , this.state.classID)
+           };
+
        updateStudyMethods = event => {
        event.preventDefault();
         console.log(event.target.dataset.studymethod)
@@ -62,7 +70,7 @@ class Questionnaire extends Component {
        render() {
         return (
             <form>
-                <p>School Name: {this.state.schoolName}</p>
+                <p>School Name: {this.state.schoolName}
                  <input 
                 type="text"
                 placeholder="School Name"
@@ -71,6 +79,7 @@ class Questionnaire extends Component {
                 value={this.state.schoolName}
                
                />
+               </p>
               <label>
                 Class Standing
                 <select value={this.state.classStanding} onChange={this.handleInputChange}>
@@ -80,6 +89,23 @@ class Questionnaire extends Component {
                     <option value="Senior">Senior</option>
                  </select>
                </label>
+               <p>Class ID: {this.state.classID} 
+                 <input
+                 type="text"
+                 placeholder="Class ID"
+                 name="classID"
+                 onChange={this.handleInputChange}
+                 value={this.state.classID}
+                  />
+               Professor: {this.state.professorName}
+                <input
+                type="text"
+                placeholder="Professor"
+                name="professorName"
+                onChange={this.handleInputChange}
+                value={this.state.professorName}
+                 />
+               </p>
                <p>Preferred study methods (select all that apply)</p>
               <button onClick={this.updateStudyMethods} data-studymethod="Flashcards" > Flashcards </button>
               <button onClick={this.updateStudyMethods} data-studymethod="Quizzes" > Quizzes </button>
@@ -95,36 +121,36 @@ class Questionnaire extends Component {
                <button onClick={this.updateStudyPlaces} data-studyplace="Home" > Home </button>
                <button onClick={this.updateStudyPlaces} data-studyplace="Other" > Other </button>
                <p>Availability</p>
-               <button onClick={this.updateAvailability} data-availability="Monday Morning" classID="Monday"> Monday Morning </button>
-               <button onClick={this.updateAvailability} data-availability="Monday Afternoon" classID="Monday" > Monday Afternoon </button>
-               <button onClick={this.updateAvailability} data-availability="Monday Evening" classID="Monday" > Monday Evening </button>
-               <button onClick={this.updateAvailability} data-availability="Monday Night" classID="Monday" > Monday Night </button>
-               <button onClick={this.updateAvailability} data-availability="Tuesday Morning" classID="Tuesday" > Tuesday Morning </button>
-               <button onClick={this.updateAvailability} data-availability="Tuesday Afternoon" classID="Tuesday" > Tuesday Afternoon </button>
-               <button onClick={this.updateAvailability} data-availability="Tuesday Evening" classID="Tuesday" > Tuesday Evening </button>
-               <button onClick={this.updateAvailability} data-availability="Tuesday Night" classID="Tuesday" > Tuesday Night </button>
-               <button onClick={this.updateAvailability} data-availability="Wednesday Morning" classID="Wednesday" > Wednesday Morning </button>
-               <button onClick={this.updateAvailability} data-availability="Wednesday Afternoon" classID="Wednesday" > Wednesday Afternoon </button>
-               <button onClick={this.updateAvailability} data-availability="Wednesday Evening" classID="Wednesday" > Wednesday Evening </button>
-               <button onClick={this.updateAvailability} data-availability="Wednesday Night" classID="Wednesday" > Wednesday Night </button>
-               <button onClick={this.updateAvailability} data-availability="Thursday Morning" classID="Thursday" > Thursday Morning </button>
-               <button onClick={this.updateAvailability} data-availability="Thursday Afternoon" classID="Thursday" > Thursday Afternoon </button>
-               <button onClick={this.updateAvailability} data-availability="Thursday Evening" classID="Thursday" > Thursday Evening </button>
-               <button onClick={this.updateAvailability} data-availability="Thursday Night" classID="Thursday" > Thursday Night </button>
-               <button onClick={this.updateAvailability} data-availability="Friday Morning" classID="Friday" > Friday Morning </button>
-               <button onClick={this.updateAvailability} data-availability="Friday Afternoon" classID="Friday" > Friday Afternoon </button>
-               <button onClick={this.updateAvailability} data-availability="Friday Evening" classID="Friday" > Friday Night </button>
-               <button onClick={this.updateAvailability} data-availability="Friday Night" classID="Friday" > Friday Evening </button>
-               <button onClick={this.updateAvailability} data-availability="Saturday Morning" classID="Saturday" > Saturday Morning </button>
-               <button onClick={this.updateAvailability} data-availability="Saturday Afternoon" classID="Saturday" > Saturday Afternoon </button>
-               <button onClick={this.updateAvailability} data-availability="Saturday Evening" classID="Saturday" > Saturday Evening </button>
-               <button onClick={this.updateAvailability} data-availability="Saturday Night" classID="Saturday" > Saturday Night </button>
-               <button onClick={this.updateAvailability} data-availability="Sunday Morning" classID="Sunday" > Sunday Morning </button>
-               <button onClick={this.updateAvailability} data-availability="Sunday Afternoon" classID="Sunday" > Sunday Afternoon </button>
-               <button onClick={this.updateAvailability} data-availability="Sunday Evening" classID="Sunday" > Sunday Evening </button>
-               <button onClick={this.updateAvailability} data-availability="Sunday Night" classID="Sunday" > Sunday Night </button>
+               <button onClick={this.updateAvailability} data-availability="Monday Morning" className="Monday"> Monday Morning </button>
+               <button onClick={this.updateAvailability} data-availability="Monday Afternoon" className="Monday" > Monday Afternoon </button>
+               <button onClick={this.updateAvailability} data-availability="Monday Evening" className="Monday" > Monday Evening </button>
+               <button onClick={this.updateAvailability} data-availability="Monday Night" className="Monday" > Monday Night </button>
+               <button onClick={this.updateAvailability} data-availability="Tuesday Morning" className="Tuesday" > Tuesday Morning </button>
+               <button onClick={this.updateAvailability} data-availability="Tuesday Afternoon" className="Tuesday" > Tuesday Afternoon </button>
+               <button onClick={this.updateAvailability} data-availability="Tuesday Evening" className="Tuesday" > Tuesday Evening </button>
+               <button onClick={this.updateAvailability} data-availability="Tuesday Night" className="Tuesday" > Tuesday Night </button>
+               <button onClick={this.updateAvailability} data-availability="Wednesday Morning" className="Wednesday" > Wednesday Morning </button>
+               <button onClick={this.updateAvailability} data-availability="Wednesday Afternoon" className="Wednesday" > Wednesday Afternoon </button>
+               <button onClick={this.updateAvailability} data-availability="Wednesday Evening" className="Wednesday" > Wednesday Evening </button>
+               <button onClick={this.updateAvailability} data-availability="Wednesday Night" className="Wednesday" > Wednesday Night </button>
+               <button onClick={this.updateAvailability} data-availability="Thursday Morning" className="Thursday" > Thursday Morning </button>
+               <button onClick={this.updateAvailability} data-availability="Thursday Afternoon" className="Thursday" > Thursday Afternoon </button>
+               <button onClick={this.updateAvailability} data-availability="Thursday Evening" className="Thursday" > Thursday Evening </button>
+               <button onClick={this.updateAvailability} data-availability="Thursday Night" className="Thursday" > Thursday Night </button>
+               <button onClick={this.updateAvailability} data-availability="Friday Morning" className="Friday" > Friday Morning </button>
+               <button onClick={this.updateAvailability} data-availability="Friday Afternoon" className="Friday" > Friday Afternoon </button>
+               <button onClick={this.updateAvailability} data-availability="Friday Evening" className="Friday" > Friday Night </button>
+               <button onClick={this.updateAvailability} data-availability="Friday Night" className="Friday" > Friday Evening </button>
+               <button onClick={this.updateAvailability} data-availability="Saturday Morning" className="Saturday" > Saturday Morning </button>
+               <button onClick={this.updateAvailability} data-availability="Saturday Afternoon" className="Saturday" > Saturday Afternoon </button>
+               <button onClick={this.updateAvailability} data-availability="Saturday Evening" className="Saturday" > Saturday Evening </button>
+               <button onClick={this.updateAvailability} data-availability="Saturday Night" className="Saturday" > Saturday Night </button>
+               <button onClick={this.updateAvailability} data-availability="Sunday Morning" className="Sunday" > Sunday Morning </button>
+               <button onClick={this.updateAvailability} data-availability="Sunday Afternoon" className="Sunday" > Sunday Afternoon </button>
+               <button onClick={this.updateAvailability} data-availability="Sunday Evening" className="Sunday" > Sunday Evening </button>
+               <button onClick={this.updateAvailability} data-availability="Sunday Night" className="Sunday" > Sunday Night </button>
                <p>Submit</p>
-               <button onClick={this.handleFormSubmit} classID="submit">Submit</button>
+               <button onClick={this.handleFormSubmit} className="submit">Submit</button>
             </form>
          ) 
 
@@ -134,7 +160,6 @@ class Questionnaire extends Component {
 
 
      export default Questionnaire;
-
 
 
 
