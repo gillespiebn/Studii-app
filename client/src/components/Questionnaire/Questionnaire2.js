5639aacccee643ec2daf1d1e96120a0a43a2d2d3
Olivia Garrett <oliviajonesgarrett@gmail.com>
2018-04-10 15:33:31 -0400

    updates to questionnaire

--- client/src/components/Questionnaire/Questionnaire2.js
+++ client/src/components/Questionnaire/Questionnaire2.js
@@ -3,11 +3,12 @@ import "./Questionnaire.css";
 import TeacherInput from "../Teachers";
 import ClassInput from "../Classes";
 import API from '../../utils/API.js';
-import { Segment, Container, Header, Icon, Label, Form, Button, Search, Select, Grid } from 'semantic-ui-react';
+import { Segment, Container, Header, Icon, Input, Label, Form, Button, Search, Select, Grid, Dropdown } from 'semantic-ui-react';
 import _ from 'lodash';
 import allSchools from '../../utils/allSchools.js'
 import twoSchools from '../../utils/twoSchools.js'
 import SearchForm from "../SearchForm";
+
 // import SearchFormRenderer from "../SearchFormRenderer";
 
 
@@ -138,8 +139,8 @@ class Questionnaire extends Component {
           times: this.state.times,
           locations: this.state.locations,
           photo: "https://images-na.ssl-images-amazon.com/images/I/71EigcnfsyL.pnghttps://images-na.ssl-images-amazon.com/images/I/71EigcnfsyL.png",
-          major: /*this.state.major*/'math',
-          minor: /*this.state.minor*/'science'
+          major: this.state.major,
+          minor: this.state.minor
         }
         // need to make check functions here. they will set states and if those states exist, they will highlight where something needs to change
         this.checkFunctions(objToSave);
@@ -182,6 +183,13 @@ class Questionnaire extends Component {
         console.log("we got here");
         API.createUser(obj).then(data => console.log(data.data)).catch(err => console.log(err));
       }
+
+      updateClassStanding = event => {
+        event.preventDefault();
+        let standing = this.state.classStanding
+        standing.push(EventTarget.dataset.classStanding)
+        this.setState({ classStanding: standing })
+      };
 
       updateStudyMethods = event => {
         event.preventDefault();
@@ -240,111 +248,124 @@ class Questionnaire extends Component {
           <Container>
             <Segment style={{ marginTop: 20}} raised>
               <Container textAlign="center">
-                <Header as="h2">Create User</Header>
+                <Header as="h2">Find a Studii Buddy</Header>
               </Container>
               <Container>
                 <Form>
-                  <Form.Field className={`${this.state.nameProblem}`}>
-                    <Label>
-                      {<Icon name="user" size="large"/>}
-                    </Label>
+                  <Form.Group>
+                    <Form.Field control={Input} label='First and Last Name' className={`${this.state.nameProblem}`} width={16}>
                     <input type="text" placeholder="Enter First and Last Name Here" required name="name" onChange={this.handleInputChange} />
                     {this.state.nameEmpty ?
-                      <Label basic color="red" pointing>{`${this.state.nameEmpty}`}</Label>
-                    : ""}
-                  </ Form.Field>
-                  <Form.Field className={`${this.state.emailProblem}`} >
-                    <Label>
-                      {<Icon name="mail" size="large"/>}
-                    </Label>
-                    <input type="email" placeholder="Enter School (.edu) Email Address Here" required name="email" onChange={this.handleInputChange} />
-                    {this.state.emailEmpty ?
-                      <Label basic color="red" pointing>{`${this.state.emailEmpty}`}</Label>
-                    : ""}
-                    {this.state.emailFormatProblem ?
-                      <Label basic color="red" pointing>{`${this.state.emailFormatProblem}`}</Label>
-                    : ""}
-                  </Form.Field>
-                  <Form.Field>
-                    <Label>
-                      {<Icon name="university" size="large" />}
-                    </Label>
-                    <div>     
-                    {/* <Search
-                      loading={this.stateisLoading}
-                      onResultSelect={this.handleResultSelect}
-                      onSearchChange={_.debounce(this.handleSearchChange, 500, { leading: true })}
-                      results={this.state.results}
-                      value={this.state.value}
-                      {...this.props}
-                    /> */}
-                    </div>
-                  </Form.Field>
-                  <Form.Field className={`${this.state.majorProblem}`}>
-                    <Label>
-                      {<Icon name="book" size="large"/>}
-                    </Label>
-                    <input type="text" placeholder="Enter Your Major" required name="major" onChange={this.handleInputChange} />
-                    {this.state.majorEmpty ?
-                      <Label basic color="red" pointing>{`${this.state.majorEmpty}`}</Label>
-                    : ""}
-                  </ Form.Field>
-                  <Form.Field className="minor">
-                    <Label>
-                      {<Icon name="book" size="large"/>}
-                    </Label>
-                    <input type="text" placeholder="Enter Your Minor (if applicable)" required name="minor" onChange={this.handleInputChange} />
-                  </ Form.Field>  
-                  <Form.Field className="class-standing" >
-                    <Form.Field control={Select} label='Class-Standing' name="classStanding" onChange={this.handleInputChange} options={classStandingOptions} placeholder='Select Your Class Standing' />   
-                  </Form.Field>
-                  <Button.Group>
-                    <Button onClick={this.updateStudyMethods} data-methods="Flashcards" > Flashcards </Button>
-                    <Button onClick={this.updateStudyMethods} data-methods="Quizzes" > Quizzes </Button>
-                    <Button onClick={this.updateStudyMethods} data-methods="Rereading" > Rereading </Button>
+                        <Label basic color="red" pointing>{`${this.state.nameEmpty}`}</Label>
+                      : ""}
+
+                      {/* <Label>
+                        {<Icon name="user" size="large"/>}
+                      </Label> */}
+                      
+                    </ Form.Field>
+                  </Form.Group>
+                  <Form.Group>  
+                    <Form.Field control={Input} label="Email Address" placeholder="Enter School (.edu) Email Address" className={`${this.state.emailProblem}`} width={16}>
+                      <input type="email" required name="email" onChange={this.handleInputChange} />
+                      {this.state.emailEmpty ?
+                        <Label basic color="red" pointing>{`${this.state.emailEmpty}`}</Label>
+                      : ""}
+                      {this.state.emailFormatProblem ?
+                        <Label basic color="red" pointing>{`${this.state.emailFormatProblem}`}</Label>
+                      : ""}
+                    </Form.Field>
+                  </Form.Group>
+                  <Form.Group>  
+                    <Form.Field>
+                      <Label>
+                        {<Icon name="university" size="large" />}
+                      </Label>
+                      <div>     
+                      {/* <Search
+                        loading={this.stateisLoading}
+                        onResultSelect={this.handleResultSelect}
+                        onSearchChange={_.debounce(this.handleSearchChange, 500, { leading: true })}
+                        results={this.state.results}
+                        value={this.state.value}
+                        {...this.props}
+                      /> */}
+                      </div>
+                    </Form.Field>  
+                  </Form.Group>
+                  <Form.Group widths='equal'>  
+                    <Form.Field control={Input} label="Major" className={`${this.state.majorProblem}`}>
+                      <input type="text" placeholder="Enter Your Major" required name="major" onChange={this.handleInputChange} />
+                      {this.state.majorEmpty ?
+                        <Label basic color="red" pointing>{`${this.state.majorEmpty}`}</Label>
+                      : ""}
+                    </ Form.Field>
+                    <Form.Field control={Input} label="Minor" className="minor">
+                      <input type="text" placeholder="Enter Your Minor (if applicable)" required name="minor" onChange={this.handleInputChange} />
+                    </ Form.Field>  
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Select label='Class Standing' options={ classStandingOptions } placeholder='Select Your Class Standing' width={16} onChange={this.handleInputChange}/>
+                  </Form.Group>
+                  <Form.Group widths="equal">
+                  <p>Preferred Method of Study</p>
+                    <Button onClick={this.updateStudyMethods} data-methods="Flashcards" width={5}> Flashcards </Button>
+                    <Button onClick={this.updateStudyMethods} data-methods="Quizzes" width={5}> Quizzes </Button>
+                    <Button onClick={this.updateStudyMethods} data-methods="Rereading" width={5}> Rereading </Button>
+                  </Form.Group>
+                  <Form.Group width="equal">  
                     <Button onClick={this.updateStudyMethods} data-methods="Revision Notes" > Revision Notes </Button>
                     <Button onClick={this.updateStudyMethods} data-methods="Mnemonics" > Mnemonics </Button>
                     <Button onClick={this.updateStudyMethods} data-methods="Other" > Other </Button>
-                  </Button.Group>
-                  <Button.Group>
+                  </Form.Group>
+                  <label>Preferred Study Location</label>
+                  <Form.Group widths="equal">
                     <Button onClick={this.updateStudyPlaces} data-locations="Library">Library</Button>
                     <Button onClick={this.updateStudyPlaces} data-locations="Coffee Shop">Coffee Shop</Button>
                     <Button onClick={this.updateStudyPlaces} data-locations="Commons">Commons</Button>
+                  </Form.Group>
+                  <Form.Group widths="equal">  
                     <Button onClick={this.updateStudyPlaces} data-locations="Cafe">Cafe</Button>
-                    <Button onClick={this.updateStudyPlaces} data-locations="Home">Cafe</Button>
-                    <Button onClick={this.updateStudyPlaces} data-locations="Online">Online</Button>
+                    <Button onClick={this.updateStudyPlaces} data-locations="Home">Home</Button>
                     <Button onClick={this.updateStudyPlaces} data-locations="Other">Other</Button>
-                  </Button.Group>
-                  <Button.Group>
-                    <Button onClick={this.updateAvailability} data-times="Monday Morning" className="Monday"> Monday Morning </Button>
-                    <Button onClick={this.updateAvailability} data-times="Monday Afternoon" className="Monday" > Monday Afternoon </Button>
-                    <Button onClick={this.updateAvailability} data-times="Monday Evening" className="Monday" > Monday Evening </Button>
-                    <Button onClick={this.updateAvailability} data-times="Monday Night" className="Monday" > Monday Night </Button>
-                    <Button onClick={this.updateAvailability} data-times="Tuesday Morning" className="Tuesday" > Tuesday Morning </Button>
-                    <Button onClick={this.updateAvailability} data-times="Tuesday Afternoon" className="Tuesday" > Tuesday Afternoon </Button>
-                    <Button onClick={this.updateAvailability} data-times="Tuesday Evening" className="Tuesday" > Tuesday Evening </Button>
-                    <Button onClick={this.updateAvailability} data-times="Tuesday Night" className="Tuesday" > Tuesday Night </Button>
-                    <Button onClick={this.updateAvailability} data-times="Wednesday Morning" className="Wednesday" > Wednesday Morning </Button>
-                    <Button onClick={this.updateAvailability} data-times="Wednesday Afternoon" className="Wednesday" > Wednesday Afternoon </Button>
-                    <Button onClick={this.updateAvailability} data-times="Wednesday Evening" className="Wednesday" > Wednesday Evening </Button>
-                    <Button onClick={this.updateAvailability} data-times="Wednesday Night" className="Wednesday" > Wednesday Night </Button>
-                    <Button onClick={this.updateAvailability} data-times="Thursday Morning" className="Thursday" > Thursday Morning </Button>
-                    <Button onClick={this.updateAvailability} data-times="Thursday Afternoon" className="Thursday" > Thursday Afternoon </Button>
-                    <Button onClick={this.updateAvailability} data-times="Thursday Evening" className="Thursday" > Thursday Evening </Button>
-                    <Button onClick={this.updateAvailability} data-times="Thursday Night" className="Thursday" > Thursday Night </Button>
-                    <Button onClick={this.updateAvailability} data-times="Friday Morning" className="Friday" > Friday Morning </Button>
-                    <Button onClick={this.updateAvailability} data-times="Friday Afternoon" className="Friday" > Friday Afternoon </Button>
-                    <Button onClick={this.updateAvailability} data-times="Friday Evening" className="Friday" > Friday Night </Button>
-                    <Button onClick={this.updateAvailability} data-times="Friday Night" className="Friday" > Friday Evening </Button>
-                    <Button onClick={this.updateAvailability} data-times="Saturday Morning" className="Saturday" > Saturday Morning </Button>
-                    <Button onClick={this.updateAvailability} data-times="Saturday Afternoon" className="Saturday" > Saturday Afternoon </Button>
-                    <Button onClick={this.updateAvailability} data-times="Saturday Evening" className="Saturday" > Saturday Evening </Button>
-                    <Button onClick={this.updateAvailability} data-times="Saturday Night" className="Saturday" > Saturday Night </Button>
+                  </Form.Group>
+                  <label>Preferred Study Time</label>
+                  <Form.Group widths="equal">
                     <Button onClick={this.updateAvailability} data-times="Sunday Morning" className="Sunday" > Sunday Morning </Button>
+                    <Button onClick={this.updateAvailability} data-times="Monday Morning" className="Monday"> Monday Morning </Button>
+                    <Button onClick={this.updateAvailability} data-times="Tuesday Morning" className="Tuesday" > Tuesday Morning </Button>
+                    <Button onClick={this.updateAvailability} data-times="Wednesday Morning" className="Wednesday" > Wednesday Morning </Button> 
+                    <Button onClick={this.updateAvailability} data-times="Thursday Morning" className="Thursday" > Thursday Morning </Button>
+                    <Button onClick={this.updateAvailability} data-times="Friday Morning" className="Friday" > Friday Morning </Button>
+                    <Button onClick={this.updateAvailability} data-times="Saturday Morning" className="Saturday" > Saturday Morning </Button>
+                  </Form.Group>
+                  <Form.Group widths="equal"> 
                     <Button onClick={this.updateAvailability} data-times="Sunday Afternoon" className="Sunday" > Sunday Afternoon </Button>
-                    <Button onClick={this.updateAvailability} data-times="Sunday Evening" className="Sunday" > Sunday Evening </Button>
+                    <Button onClick={this.updateAvailability} data-times="Monday Afternoon" className="Monday" > Monday Afternoon </Button>
+                    <Button onClick={this.updateAvailability} data-times="Tuesday Afternoon" className="Tuesday" > Tuesday Afternoon </Button> 
+                    <Button onClick={this.updateAvailability} data-times="Wednesday Afternoon" className="Wednesday" > Wednesday Afternoon </Button>
+                    <Button onClick={this.updateAvailability} data-times="Thursday Afternoon" className="Thursday" > Thursday Afternoon </Button>
+                    <Button onClick={this.updateAvailability} data-times="Friday Afternoon" className="Friday" > Friday Afternoon </Button>
+                    <Button onClick={this.updateAvailability} data-times="Saturday Afternoon" className="Saturday" > Saturday Afternoon </Button>
+                  </Form.Group>
+                  <Form.Group widths="equal">
+                    <Button onClick={this.updateAvailability} data-times="Sunday Evening" className="Sunday" > Sunday Evening </Button> 
+                    <Button onClick={this.updateAvailability} data-times="Monday Evening" className="Monday" > Monday Evening </Button>   
+                    <Button onClick={this.updateAvailability} data-times="Tuesday Evening" className="Tuesday" > Tuesday Evening </Button>
+                    <Button onClick={this.updateAvailability} data-times="Wednesday Evening" className="Wednesday" > Wednesday Evening </Button>
+                    <Button onClick={this.updateAvailability} data-times="Thursday Evening" className="Thursday" > Thursday Evening </Button>
+                    <Button onClick={this.updateAvailability} data-times="Friday Evening" className="Friday" > Friday Evening </Button>
+                    <Button onClick={this.updateAvailability} data-times="Saturday Evening" className="Saturday" > Saturday Evening </Button>
+                  </Form.Group>
+                  <Form.Group widths="equal">   
                     <Button onClick={this.updateAvailability} data-times="Sunday Night" className="Sunday" > Sunday Night </Button>
-                  </Button.Group>  
+                    <Button onClick={this.updateAvailability} data-times="Monday Night" className="Monday" > Monday Night </Button>
+                    <Button onClick={this.updateAvailability} data-times="Tuesday Night" className="Tuesday" > Tuesday Night </Button>
+                    <Button onClick={this.updateAvailability} data-times="Wednesday Night" className="Wednesday" > Wednesday Night </Button>
+                    <Button onClick={this.updateAvailability} data-times="Thursday Night" className="Thursday" > Thursday Night </Button>
+                    <Button onClick={this.updateAvailability} data-times="Friday Night" className="Friday" > Friday Night </Button>
+                    <Button onClick={this.updateAvailability} data-times="Saturday Night" className="Saturday" > Saturday Night </Button>
+                  </Form.Group>  
                 </Form>
               </Container>
 
@@ -354,7 +375,7 @@ class Questionnaire extends Component {
               </input> */}
               {/* <Form.Input label='State' name="state" control={Select} options={states} onChange={this.handleInputChange} placeholder='Select Your State'/> */}
 
-              <Grid>
+              {/* <Grid>
                 <Grid.Column width={3}>
                   <Form.Field className={`${this.state.nameProblem}`}>
                     <Label>
@@ -376,11 +397,11 @@ class Questionnaire extends Component {
                     {...this.props}  
                   />
                 </Grid.Column>
-              </Grid>
+              </Grid> */}
 
               {/* this button is just kind of a placeholder. it works, but probably needs styling */}
               <p>Submit</p>
-              <button onClick={this.handleFormSubmit} className="submit">Submit</button>
+              <Button onClick={this.handleFormSubmit} className="submit">Submit</Button>
             </Segment>
           </Container>
         )
