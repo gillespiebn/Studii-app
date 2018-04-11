703770e2d6e5bbbe9b7636e5d2b82c2e59960551
Brittany <brittanygillespie31@gmail.com>
2018-04-10 21:08:07 -0400

    Calendar component update: image toggle, changed buttons to images

--- client/src/components/Calendar/Calendar.js
+++ client/src/components/Calendar/Calendar.js
@@ -1,29 +1,81 @@
-var SunMor = React.createClass({
-    getInitialState: function() {
+import React, { Component } from "react";
+import "./Calendar.css";
+
+class Calendar extends Component {
+
+    state = {
+        src: ""
+    };
+    
+    getInitialState = () => {
       return {
-        src: 'pinkMor.png'
+        src: "/images/pinkMor.png"
       }
-      
-    },
+    };
     
-    handleClick: function() {
-      this.setState({
-        src: '../../client\public\images\blueMor.png'
-      })
-    },
-    
-    render : function() {
+    morningAvail = () => {
+        if(this.state.src === "/images/pinkMor.png") {
+            this.setState({src:"/images/blueMor.png"})
+        } else {
+            this.setState({src:"/images/pinkMor.png"})
+        }
+    };
+    afternoonAvail = () => {
+        if(this.state.src === "/images/pinkAft.png") {
+            this.setState({src:"/images/blueAft.png"})
+        } else {
+            this.setState({src:"/images/pinkAft.png"})
+        }
+    };
+    eveningAvail = () => {
+        if(this.state.src === "/images/pinkEve.png") {
+            this.setState({src:"/images/blueEve.png"})
+        } else {
+            this.setState({src:"/images/pinkEve.png"})
+        }
+    };
+    nightAvail = () => {
+        if(this.state.src === "/images/pinkNi.png") {
+            this.setState({src:"/images/blueNi.png"})
+        } else {
+            this.setState({src:"/images/pinkNi.png"})
+        }
+    };
+
+    render() {
       return (
-        <div>
-          <button 
-            onClick={this.handleClick} 
-            style={{backgroundColor:this.state.bgColor}}>Button</button>
-        </div>
+            <div>
+              <img src={this.state.src} onClick={this.updateAvailability} data-times="Monday Morning" className="Monday" />
+              <img src={this.state.src} onClick={this.updateAvailability} data-times="Monday Afternoon" className="Monday" />
+              <img src={this.state.src} onClick={this.updateAvailability} data-times="Monday Evening" className="Monday" />
+              <img src={this.state.src} onClick={this.updateAvailability} data-times="Monday Night" className="Monday" />
+              <img src={this.state.src} onClick={this.updateAvailability} data-times="Tuesday Morning" className="Tuesday" />
+              <img src={this.state.src} onClick={this.updateAvailability} data-times="Tuesday Afternoon" className="Tuesday" />
+              <img src={this.state.src} onClick={this.updateAvailability} data-times="Tuesday Evening" className="Tuesday" />
+              <img src={this.state.src} onClick={this.updateAvailability} data-times="Tuesday Night" className="Tuesday" />
+              <img src={this.state.src} onClick={this.updateAvailability} data-times="Wednesday Morning" className="Wednesday" />
+              <img src={this.state.src} onClick={this.updateAvailability} data-times="Wednesday Afternoon" className="Wednesday" />
+              <img src={this.state.src} onClick={this.updateAvailability} data-times="Wednesday Evening" className="Wednesday" />
+              <img src={this.state.src} onClick={this.updateAvailability} data-times="Wednesday Night" className="Wednesday" />
+              <img src={this.state.src} onClick={this.updateAvailability} data-times="Thursday Morning" className="Thursday" />
+              <img src={this.state.src} onClick={this.updateAvailability} data-times="Thursday Afternoon" className="Thursday" />
+              <img src={this.state.src} onClick={this.updateAvailability} data-times="Thursday Evening" className="Thursday" />
+              <img src={this.state.src} onClick={this.updateAvailability} data-times="Thursday Night" className="Thursday" />
+              <img src={this.state.src} onClick={this.updateAvailability} data-times="Friday Morning" className="Friday" />
+              <img src={this.state.src} onClick={this.updateAvailability} data-times="Friday Afternoon" className="Friday" />
+              <img src={this.state.src} onClick={this.updateAvailability} data-times="Friday Evening" className="Friday" />
+              <img src={this.state.src} onClick={this.updateAvailability} data-times="Friday Night" className="Friday" />
+              <img src={this.state.src} onClick={this.updateAvailability} data-times="Saturday Morning" className="Saturday" />
+              <img src={this.state.src} onClick={this.updateAvailability} data-times="Saturday Afternoon" className="Saturday" />
+              <img src={this.state.src} onClick={this.updateAvailability} data-times="Saturday Evening" className="Saturday" />
+              <img src={this.state.src} onClick={this.updateAvailability} data-times="Saturday Night" className="Saturday" />
+              <img src={this.state.src} onClick={this.updateAvailability} data-times="Sunday Morning" className="Sunday" />
+              <img src={this.state.src} onClick={this.updateAvailability} data-times="Sunday Afternoon" className="Sunday" />
+              <img src={this.state.src} onClick={this.updateAvailability} data-times="Sunday Evening" className="Sunday" />
+              <img src={this.state.src} onClick={this.updateAvailability} data-times="Sunday Night" className="Sunday" />
+            </div>
       )
     }
-  });
+  }
   
-  ReactDOM.render(
-    <Button />,
-      document.getElementById('button')
-  );
+  export default Calendar;
