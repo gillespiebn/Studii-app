b076ac40642a26879a253d009f5fecb7547e12d5
Brittany <brittanygillespie31@gmail.com>
2018-04-15 21:27:44 -0400

    swipe card fixes

--- client/src/components/SwipeCard/SwipeCard.js
+++ client/src/components/SwipeCard/SwipeCard.js
@@ -7,53 +7,18 @@ import API from "../../utils/API";
 
 import { Card, Image, Container, Button, List, Transition } from 'semantic-ui-react';
 
-class Profiles extends React.Component {  
+class SwipeCard extends React.Component {  
 
     state = {
        user: this.props.user,
-       matches: this.props.matches,
-       facebook_id: this.props.facebook_id,
-     
+       matches: this.props.matches,     
      }
-
-     componentDidMount() {
-        this.getUser();
-        this.getMatches();
-        console.log("there is a fairy");
-
-    }
- 
-
-       getUser = () => {
-         API.getUser(this.props.facebook_id)
-            .then(data => {
-                this.setState({user: data.data[0]})
-                console.log(data)
-                this.getMatches()
-
-        }
-        )
-    };
-
-    getMatches = () => {
-     API.getMatches(this.state.user)
-     .then(data => {
-       console.log("the line under this is matches")
-       console.log(data)
-      
-     }
-   )}
-
-
 
     render() {
-      console.log("at render " + this.state.matches.length);
-       var firstUserId=this.props.matches[0].facebook_id;
-      if (this.state.matches) {
       return(
-             {idk: this.state.matches.map(profile => (
+             
               <Container>
-                <Card className="cardContainer" fluid align="center" key={profile._id} style={{display: (firstUserId===profile.facebook_id ? 'block' : 'none') }}>
+                <Card className="cardContainer" fluid align="center">
                 <div>
                 <ProfileHeader />
                 <div className="sectionTitle">Classes</div>
@@ -68,7 +33,7 @@ class Profiles extends React.Component {
               ))};
               </Card>
               </Container>
-  ))}
-        )}}
+)
+}
     };
   export default SwipeCard;
