a71dde07b20026fbe1c8372b62158847e25f190f
Brittany <brittanygillespie31@gmail.com>
2018-04-15 21:33:13 -0400

    commenting this out

--- client/src/components/SwipeCard/SwipeCard.js
+++ client/src/components/SwipeCard/SwipeCard.js
@@ -1,39 +1,39 @@
-import React from "react";
-import "./SwipeCard.css";
-import ProfileHeader from "../../components/ProfileHeader";
-import _ from 'lodash';
-import Footer from "../Footer";
-import API from "../../utils/API";
+// import React from "react";
+// import "./SwipeCard.css";
+// import ProfileHeader from "../../components/ProfileHeader";
+// import _ from 'lodash';
+// import Footer from "../Footer";
+// import API from "../../utils/API";
 
-import { Card, Image, Container, Button, List, Transition } from 'semantic-ui-react';
+// import { Card, Image, Container, Button, List, Transition } from 'semantic-ui-react';
 
-class SwipeCard extends React.Component {  
+// class SwipeCard extends React.Component {  
 
-    state = {
-       user: this.props.user,
-       matches: this.props.matches,     
-     }
+//     state = {
+//        user: this.props.user,
+//        matches: this.props.matches,     
+//      }
 
-    render() {
-      return(
+//     render() {
+//       return(
              
-              <Container>
-                <Card className="cardContainer" fluid align="center">
-                <div>
-                <ProfileHeader />
-                <div className="sectionTitle">Classes</div>
-                  {profile.classes}
-                <div className="sectionTitle">Preferred study methods</div>
-                  {profile.methods}
-                <div className="sectionTitle">Preferred places to study</div>
-                  {profile.locations}
-                <div className="sectionTitle">Availability</div>
-                  {profile.times}
-                </div>
-              ))};
-              </Card>
-              </Container>
-)
-}
-    };
-  export default SwipeCard;
+//               <Container>
+//                 <Card className="cardContainer" fluid align="center">
+//                 <div>
+//                 <ProfileHeader />
+//                 <div className="sectionTitle">Classes</div>
+//                   {profile.classes}
+//                 <div className="sectionTitle">Preferred study methods</div>
+//                   {profile.methods}
+//                 <div className="sectionTitle">Preferred places to study</div>
+//                   {profile.locations}
+//                 <div className="sectionTitle">Availability</div>
+//                   {profile.times}
+//                 </div>
+//               ))};
+//               </Card>
+//               </Container>
+// )
+// }
+//     };
+//   export default SwipeCard;
